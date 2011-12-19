eb82d88572c8ce3793b2eb8bd7c2f4c992da0ffc
Daniel Watkins <daniel@daniel-watkins.co.uk>
2011-12-19 17:55:06 +0000

    Modify view_ticket test to test that settings are used.

--- centipede_ui/tickets/tests/unit/test_views.py
+++ centipede_ui/tickets/tests/unit/test_views.py
@@ -4,6 +4,7 @@ from nose.tools import assert_equal
 from centipede_ui.tickets.views import view_ticket
 
 
+@patch('centipede_ui.tickets.views.settings.CENTIPEDE_URL', 'MockURL')
 @patch('centipede_ui.tickets.views.render_to_response')
 @patch('centipede_ui.tickets.views.Centipede')
 def test_view_ticket(centipede, render_to_response):
@@ -14,7 +15,7 @@ def test_view_ticket(centipede, render_to_response):
         'state': 'MockState',
         }
     ret = view_ticket(Mock(), 'US123')
-    assert_equal([(('http://centipede',), {})], centipede.call_args_list)
+    assert_equal([(('MockURL',), {})], centipede.call_args_list)
     assert_equal([(('US123',), {})],
                  centipede.return_value.get_ticket.call_args_list)
     assert_equal([(('tickets/view.html',
