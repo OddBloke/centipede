2d8feda6c54243659b769321ba767f5ea8212492
Daniel Watkins <daniel@daniel-watkins.co.uk>
2011-12-19 17:56:39 +0000

    Use the CENTIPEDE_URL setting when connecting to Centipede.

--- centipede_ui/settings.py
+++ centipede_ui/settings.py
@@ -128,3 +128,6 @@ LOGGING = {
         },
     }
 }
+
+
+CENTIPEDE_URL = ''
--- centipede_ui/tickets/views.py
+++ centipede_ui/tickets/views.py
@@ -1,9 +1,10 @@
+from django.conf import settings
 from django.shortcuts import render_to_response
 
 from centipede_ui.centipedelib import Centipede
 
 
 def view_ticket(request, ticket_id):
-    centipede = Centipede('http://centipede')
+    centipede = Centipede(settings.CENTIPEDE_URL)
     ticket_dict = centipede.get_ticket(ticket_id)
     return render_to_response('tickets/view.html', ticket_dict)
