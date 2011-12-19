c30d64b55bf66872e0bff5660e846f8be3a4b446
Daniel Watkins <daniel@daniel-watkins.co.uk>
2011-12-19 14:36:13 +0000

    Hand Rally story of to (stub) conversion function.

--- centipede/plugins/rally.py
+++ centipede/plugins/rally.py
@@ -3,7 +3,12 @@ from pyrally.client import RallyAPIClient
 from centipede.plugins import PluginInterface
 
 
+def get_ticket_from_rally_object():
+    pass
+
+
 class Rally(PluginInterface):
 
     def get(self, ticket_id):
-        RallyAPIClient('', '').get_story_by_name(ticket_id)
+        story = RallyAPIClient('', '').get_story_by_name(ticket_id)
+        return get_ticket_from_rally_object(story)
