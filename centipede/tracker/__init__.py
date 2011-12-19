76acdea9e9739555337d20ce5b8dee822390810d
Ondrej Kohout <ondrej.kohout@gmail.com>
2011-12-19 17:59:53 +0000

    Tracker loader

--- centipede/tracker/__init__.py
+++ centipede/tracker/__init__.py
@@ -1,3 +1,11 @@
+import sys
+
+
+def load_tracker(tracker_name):
+    module, class_name = tracker_name.rsplit('.', 1)
+    __import__(module)
+    return getattr(sys.modules[module], class_name)()
+
 class TrackerInterface(object):
 
     def get(self, ticket_id):
--- centipede/tracker/tests/unit/test_tracker.py
+++ centipede/tracker/tests/unit/test_tracker.py
@@ -1,7 +1,10 @@
-from nose.tools import assert_raises
+from nose.tools import assert_raises, assert_true
 
-from centipede.tracker import TrackerInterface
+from centipede.tracker import load_tracker, TrackerInterface
 
+def test_import_tracker():
+    tracker = load_tracker('centipede.plugins.rally.Rally')
+    assert_true(isinstance(tracker, TrackerInterface))
 
 def test_get():
     assert_raises(NotImplementedError, TrackerInterface().get, 'id')
