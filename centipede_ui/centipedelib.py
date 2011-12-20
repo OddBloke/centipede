d709e9065ab25ee3b1c076f2f3874f2db457c4f2
Daniel Watkins <daniel@daniel-watkins.co.uk>
2011-12-20 10:41:03 +0000

    Implement Centipede.get_ticket_children.

--- centipede_ui/centipedelib.py
+++ centipede_ui/centipedelib.py
@@ -15,3 +15,8 @@ class Centipede(object):
     def get_ticket(self, ticket_id):
         response = requests.get(urljoin(self.url, 'tickets', ticket_id))
         return json.loads(response.content)
+
+    def get_ticket_children(self, ticket_id):
+        response = requests.get(urljoin(self.url, 'tickets', ticket_id,
+                                        'tickets'))
+        return json.loads(response.content)
