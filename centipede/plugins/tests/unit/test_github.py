6c9ada87b124c734e6d195a051c02e2fc2270e17
Daniel Watkins <daniel@daniel-watkins.co.uk>
2011-12-20 00:00:18 +0000

    Update test to expect repository as argument to GitHub tracker.

--- centipede/plugins/tests/unit/test_github.py
+++ centipede/plugins/tests/unit/test_github.py
@@ -8,11 +8,11 @@ from centipede.plugins.tests.unit.test_rally import assert_called_once
 @patch('centipede.plugins.github.client.Github')
 @patch('centipede.plugins.github.get_ticket_from_issue')
 def test_get_ticket(get_ticket_from_issue, github_lib):
-    github = GitHub()
+    github = GitHub('MockUser/MockRepo')
     ret = github.get_ticket('MockTicketNumber')
     assert_called_once(github_lib)
     assert_called_once(github_lib.return_value.issues.show,
-                        ('MockPerson/MockRepo', 'MockTicketNumber'))
+                        ('MockUser/MockRepo', 'MockTicketNumber'))
     assert_called_once(
         get_ticket_from_issue,
         (github_lib.return_value.issues.show.return_value,), {})
