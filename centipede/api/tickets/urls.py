e8b5b773c48db1fabd0999636ba981ad86bd79a2
Daniel Watkins <daniel@daniel-watkins.co.uk>
2011-12-20 10:25:20 +0000

    Implement listing of children in API.

--- centipede/api/tickets/urls.py
+++ centipede/api/tickets/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls.defaults import patterns, url
 
-from centipede.api.tickets.views import TicketView
+from centipede.api.tickets.views import TicketChildView, TicketView
 
 
 urlpatterns = patterns('',
     #url(r'^$', TicketListView.as_view()),
     url(r'^(?P<ticket_id>\w+)/$', TicketView.as_view(), name='ticket'),
+    url(r'^(?P<ticket_id>\w+)/tickets/$', TicketChildView.as_view(),
+            name='ticket_children'),
 )
--- centipede/api/tickets/views.py
+++ centipede/api/tickets/views.py
@@ -9,6 +9,18 @@ from centipede.tracker import load_tracker
 tracker = load_tracker(settings.TRACKER)
 
 
+class TicketChildView(View):
+
+    def get(self, request, ticket_id):
+        children = tracker.list_children(ticket_id)
+        return HttpResponse(anyjson.serialize(
+            [dict(title=ticket.title,
+                  description=ticket.description,
+                  state=ticket.state,
+                  owner=ticket.owner)
+                for ticket in children]))
+
+
 class TicketView(View):
 
     def serialize(self, data):
