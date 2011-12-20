7e2051bdf907202171232bacb7b80c878f1a6eaf
Daniel Watkins <daniel@daniel-watkins.co.uk>
2011-12-20 15:08:21 +0000

    Add test for TicketView.

---
+++ centipede/api/tickets/tests/unit/test_views.py
@@ -0,0 +1,17 @@
+from mock import Mock, patch
+from nose.tools import assert_equal
+
+from centipede.api.tickets.views import TicketView
+from centipede.plugins.tests.unit.test_rally import assert_called_once
+
+
+@patch('centipede.api.tickets.views.HttpResponse')
+@patch('centipede.api.tickets.views.tracker')
+@patch('centipede.api.tickets.views.anyjson.serialize')
+def test_ticketview(serialize, tracker, response):
+    ret = TicketView().get(Mock(), 'US123')
+    assert_called_once(tracker.get_ticket, ('US123',))
+    assert_called_once(serialize,
+                       (tracker.get_ticket.return_value.as_dict.return_value,))
+    assert_called_once(response, (serialize.return_value,))
+    assert_equal(response.return_value, ret)
