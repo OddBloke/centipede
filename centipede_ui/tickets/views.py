bde15a8bcc3eb2881758bc61da6e21d6bdce79ec
Daniel Watkins <daniel@daniel-watkins.co.uk>
2011-12-20 14:15:29 +0000

    Handle DeadbeatTicket in view_ticket view.

--- centipede_ui/tickets/views.py
+++ centipede_ui/tickets/views.py
@@ -1,11 +1,14 @@
 from django.conf import settings
 from django.shortcuts import render_to_response
 
-from centipede_ui.centipedelib import Centipede
+from centipede_ui.centipedelib import Centipede, DeadbeatTicket
 
 
 def view_ticket(request, ticket_id):
     centipede = Centipede(settings.CENTIPEDE_URL)
     ticket_dict = centipede.get_ticket(ticket_id)
-    ticket_dict['children'] = centipede.get_ticket_children(ticket_id)
+    try:
+        ticket_dict['children'] = centipede.get_ticket_children(ticket_id)
+    except DeadbeatTicket:
+        pass
     return render_to_response('tickets/view.html', ticket_dict)
