ed4466cbd38ed526f662d578a3afb2624585ae20
Daniel Watkins <daniel@daniel-watkins.co.uk>
2011-12-20 15:23:58 +0000

    Handle IAmSterile in the API, closing #14.

--- centipede/api/tickets/views.py
+++ centipede/api/tickets/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from django.conf import settings
 
 from centipede.tracker import load_tracker
+from centipede.tracker.entities import IAmSterile
 
 
 tracker = load_tracker(settings.TRACKER)
@@ -12,7 +13,10 @@ tracker = load_tracker(settings.TRACKER)
 class TicketChildView(View):
 
     def get(self, request, ticket_id):
-        children = tracker.list_children(ticket_id)
+        try:
+            children = tracker.list_children(ticket_id)
+        except IAmSterile:
+            return HttpResponse(status=404)
         return HttpResponse(anyjson.serialize(
             [ticket.as_dict() for ticket in children]))
 
