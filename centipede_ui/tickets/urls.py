3f69e012aa5b563e044d7de8e8e7300187683fe3
Daniel Watkins <daniel@daniel-watkins.co.uk>
2011-12-20 11:48:18 +0000

    Link to child tickets.

--- centipede_ui/tickets/urls.py
+++ centipede_ui/tickets/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls.defaults import patterns, url
 
 
 urlpatterns = patterns('centipede_ui.tickets.views',
-    url(r'^view/(?P<ticket_id>.*)/$', 'view_ticket'),
+    url(r'^view/(?P<ticket_id>.*)/$', 'view_ticket', name='view_ticket'),
 )
