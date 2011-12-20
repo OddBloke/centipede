b112840fffe6dc134270e90522cc16e157d52545
Daniel Watkins <daniel@daniel-watkins.co.uk>
2011-12-20 10:39:26 +0000

    Add test for centipedelib.Centipede.get_ticket_children.

--- centipede_ui/tests/unit/test_centipedelib.py
+++ centipede_ui/tests/unit/test_centipedelib.py
@@ -15,3 +15,16 @@ def test_get_ticket(get, loads):
     assert_equal({}, kwargs)
     assert_equal([((get.return_value.content,), {})], loads.call_args_list)
     assert_equal(loads.return_value, ret)
+
+
+@patch('centipede_ui.centipedelib.json.loads')
+@patch('centipede_ui.centipedelib.requests.get')
+def test_get_ticket_children(get, loads):
+    centipede = Centipede('http://centipede')
+    ret = centipede.get_ticket_children('US123')
+    assert_equal(1, get.call_count)
+    args, kwargs = get.call_args
+    assert_equal(('http://centipede/tickets/US123/tickets/',), args)
+    assert_equal({}, kwargs)
+    assert_equal([((get.return_value.content,), {})], loads.call_args_list)
+    assert_equal(loads.return_value, ret)
