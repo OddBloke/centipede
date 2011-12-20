5693087a37aca6b67b12238caf2044b247cacfcd
Daniel Watkins <daniel@daniel-watkins.co.uk>
2011-12-20 14:10:28 +0000

    Update centipedelib tests for 404 for children.

--- centipede_ui/tests/unit/test_centipedelib.py
+++ centipede_ui/tests/unit/test_centipedelib.py
@@ -1,7 +1,7 @@
 from mock import patch
-from nose.tools import assert_equal
+from nose.tools import assert_equal, assert_raises
 
-from centipede_ui.centipedelib import Centipede
+from centipede_ui.centipedelib import Centipede, DeadbeatTicket
 
 
 @patch('centipede_ui.centipedelib.json.loads')
@@ -20,6 +20,7 @@ def test_get_ticket(get, loads):
 @patch('centipede_ui.centipedelib.json.loads')
 @patch('centipede_ui.centipedelib.requests.get')
 def test_get_ticket_children(get, loads):
+    get.return_value.status_code = 200
     centipede = Centipede('http://centipede')
     ret = centipede.get_ticket_children('US123')
     assert_equal(1, get.call_count)
@@ -28,3 +29,10 @@ def test_get_ticket_children(get, loads):
     assert_equal({}, kwargs)
     assert_equal([((get.return_value.content,), {})], loads.call_args_list)
     assert_equal(loads.return_value, ret)
+
+
+@patch('centipede_ui.centipedelib.requests.get')
+def test_get_ticket_children_unsupported(get):
+    get.return_value.status_code = 404
+    centipede = Centipede('http://centipede')
+    assert_raises(DeadbeatTicket, centipede.get_ticket_children, 'US123')
