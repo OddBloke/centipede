91d84340a7b7ec309e83ba025686aaba59085be4
Daniel Watkins <daniel@daniel-watkins.co.uk>
2011-12-20 10:49:41 +0000

    Update view unit test to expect children.

--- centipede_ui/tickets/tests/unit/test_views.py
+++ centipede_ui/tickets/tests/unit/test_views.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 from mock import Mock, patch
 from nose.tools import assert_equal
 
@@ -8,17 +10,21 @@ from centipede_ui.tickets.views import view_ticket
 @patch('centipede_ui.tickets.views.render_to_response')
 @patch('centipede_ui.tickets.views.Centipede')
 def test_view_ticket(centipede, render_to_response):
-    centipede.return_value.get_ticket.return_value = {
+    ticket_dict = {
         'title': 'MockTitle',
         'description': 'MockDescription',
         'owner': 'MockOwner',
         'state': 'MockState',
         }
+    centipede.return_value.get_ticket.return_value = ticket_dict
+    child_list = ['child1', 'child2']
+    centipede.return_value.get_ticket_children.return_value = child_list
     ret = view_ticket(Mock(), 'US123')
     assert_equal([(('MockURL',), {})], centipede.call_args_list)
     assert_equal([(('US123',), {})],
                  centipede.return_value.get_ticket.call_args_list)
-    assert_equal([(('tickets/view.html',
-                    centipede.return_value.get_ticket.return_value), {})],
+    expected_ticket_dict = deepcopy(ticket_dict)
+    expected_ticket_dict['children'] = child_list
+    assert_equal([(('tickets/view.html', expected_ticket_dict), {})],
                  render_to_response.call_args_list)
     assert_equal(render_to_response.return_value, ret)
